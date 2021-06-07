51b0ad57928256293bfab5c31a63fb137c9edc53
xiaoyuhang1 <xiaoyuhang1@yy.com>
2021-06-08 00:20:22 +0800

    fix

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210608.0004';
+const cacheName = 'chirpy-20210608.0020';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
