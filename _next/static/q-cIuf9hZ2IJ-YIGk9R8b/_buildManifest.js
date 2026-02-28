820fd5ee8348756324f2ef2c750a61bab3c09088
littlemoon-zh <littlemoon-zh@users.noreply.github.com>
2026-02-28 17:16:19 +0000

    deploy: 64633f0c03d96c9ff1146895103f05b2cc6fa02a

--- _next/static/q-cIuf9hZ2IJ-YIGk9R8b/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c4bb0c9e5c7f996b.css","static/chunks/pages/index-963bdc9b3090548b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
